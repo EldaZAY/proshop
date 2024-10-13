130ca7a07cf25e8340848b24383f23a42d144190
EldaZAY <elda.aiyuz@gmail.com>
2024-10-12 20:42:10 -0700

    rating

---
+++ frontend/src/components/Rating.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { FaStar, FaStarHalfAlt, FaRegStar } from "react-icons/fa";
+
+const Rating = ({ value, text }) => {
+  const renderStar = (index) => {
+    if (value >= index) {
+      return <FaStar />;
+    } else if (value >= index - 0.5) {
+      return <FaStarHalfAlt />;
+    } else {
+      return <FaRegStar />;
+    }
+  };
+
+  return (
+    <div className="rating">
+      {[1, 2, 3, 4, 5].map((star) => (
+        <span key={star}>{renderStar(star)}</span>
+      ))}
+      <span className="rating-text">{text && text}</span>
+    </div>
+  );
+};
+
+export default Rating;
