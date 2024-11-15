be729305f725ada1d76aff43a0ceae93e4b0ff81
EldaZAY <elda.aiyuz@gmail.com>
2024-11-14 21:10:40 -0800

    login auth middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -11,13 +11,14 @@ import {
     deleteUser, 
     updateUser 
 } from '../controllers/userController.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
 
 
-router.route('/').post(registerUser).get(getUsers);
-router.post('/login', authUser);
+router.route('/').post(registerUser).get(protect, admin, getUsers);
+router.post('/auth', authUser);
 router.post('/logout', logoutUser);
-router.route('/profile').get(getUserProfile).put(updateUserProfile);
-router.route('/:id').get(getUserById).delete(deleteUser).put(updateUser);
+router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile);
+router.route('/:id').get(protect, admin, getUserById).delete(protect, admin, deleteUser).put(protect, admin, updateUser);
 
 
 export default router;
