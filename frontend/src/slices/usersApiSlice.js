2d7ca155116765e39992901f268124223665721e
EldaZAY <elda.aiyuz@gmail.com>
2024-12-02 17:27:30 -0800

    delete user

--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -39,6 +39,13 @@ export const usersApiSlice = apiSlice.injectEndpoints({
             providesTags: ['Users'], 
             keepUnusedDataFor: 5,
         }),
+        deleteUser: builder.mutation({
+            query: (id) => ({
+                url: `${USERS_URL}/${id}`,
+                method: 'DELETE',
+            }),
+            invalidatesTags: ['Users'],
+        }),
     }),      
 });
 
@@ -48,4 +55,5 @@ export const {
     useRegisterMutation, 
     useProfileMutation,
     useGetUsersQuery,
+    useDeleteUserMutation,
 } = usersApiSlice;
