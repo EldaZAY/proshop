0443da9964fc9173b2f67222b807e8939d73dac6
EldaZAY <elda.aiyuz@gmail.com>
2024-12-02 21:40:19 -0800

    prep for production

--- backend/server.js
+++ backend/server.js
@@ -22,9 +22,6 @@ app.use(express.urlencoded({ extended: true }));
 //cookie parser middleware
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-  res.send('API is running...'); 
-});
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
@@ -35,6 +32,19 @@ app.get('/api/config/paypal', (req, res) => res.send({clientId: process.env.PAYP
 
 const __dirname = path.resolve(); // Set __dirname to the current directory
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+  // any route that is not an API route will point to the frontend index.html
+  app.get('*', (req, res) => res.sendFile
+  (path.resolve(__dirname, 'frontend', 'build', 'index.html')));
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...');
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
